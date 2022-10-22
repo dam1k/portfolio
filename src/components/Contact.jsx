d467ce5573bacc520f33f16a3c5476c2e9502ca9
Пользователь <tracacore123456789@gmail.com>
2022-10-22 15:52:40 +0300

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,12 @@ const Contact = () => {
     <div className="blue-border"></div>
     <p className="section-desc">Leave me a message and let's work together!</p>
     
-    <form className="form-container">
-      <input className="input" type="email" placeholder="Enter your Email"/>
-      <input className="input" type="text" placeholder="Enter your Name"/>
-      <textarea className="message" placeholder="Enter your message"/>
+    <form action="https://formsubmit.co/damianbostan12@gmail.com" 
+    method="POST" 
+    className="form-container">
+      <input className="input" type="email" placeholder="Enter your Email" required/>
+      <input className="input" type="text" placeholder="Enter your Name" required/>
+      <textarea className="message" placeholder="Enter your message" required/>
       <button type="submit" className="blue-btn btn">Send Email</button>
     </form>
     </section>
