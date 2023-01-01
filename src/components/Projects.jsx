0876ed918978b07008bc60f7efc9173466daeba6
dam1k <tracacore123456789@gmail.com>
2023-01-01 19:39:24 +0200

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,4 @@
-import Project1 from "../assets/img/project1.jpeg"
-import Project2 from "../assets/img/project2.jpeg"
-import Project3 from "../assets/img/project3.jpeg"
-import Project4 from "../assets/img/project4.jpeg"
+import {projects} from "../data/data";
 
 const Projects = () => {
   return (
@@ -13,69 +10,26 @@ const Projects = () => {
       
       <div className="grid-container">
 
-      <a href="https://euromobila.vercel.app/" target="_blank">
+         {projects.map(project => {
+          return (
+            <a href={project.url} target="_blank">
         <div className="grid-item">
           <div className="grid-item-img">
-            <img src={Project4}/>
+          <img src={project.image}/>
           </div>
           <div className="grid-item-info">
             <h3 className="grid-item-title">
-              Euromobila 
+              {project.title}
             </h3>
             <p className="grid-item-desc">
-            Website for Euromobila
+            {project.description} 
             </p>
           </div>
         </div>
         </a>
+          )
+         })}
 
-       <a href="https://house-marketplace-two-gules.vercel.app/" target="_blank">
-        <div className="grid-item">
-          <div className="grid-item-img">
-            <img src={Project1}/>
-          </div>
-          <div className="grid-item-info">
-            <h3 className="grid-item-title">
-              House Marketplace
-            </h3>
-            <p className="grid-item-desc">
-            On this website you can explore house listings by category, as well as log in and add your own listings.
-            </p>
-          </div>
-        </div>
-        </a>
-
-         <a href="https://expensetrack.vercel.app" target="_blank">
-        <div className="grid-item">
-          <div className="grid-item-img">
-          <img src={Project2}/>
-          </div>
-          <div className="grid-item-info">
-            <h3 className="grid-item-title">
-              Expense Tracker
-            </h3>
-            <p className="grid-item-desc">
-            Expense tracker app made with ReactJs. Monitor your expenses by adding transactions. 
-            </p>
-          </div>
-        </div>
-        </a>
-
-<a href="https://vacation-website.vercel.app" target="_blank">
-        <div className="grid-item">
-          <div className="grid-item-img">
-          <img src={Project3}/>
-          </div>
-          <div className="grid-item-info">
-            <h3 className="grid-item-title">
-              Vacation Website
-            </h3>
-            <p className="grid-item-desc">
-            Responsive landing page website made with ReactJs. 
-            </p>
-          </div>
-        </div>
-        </a>
       </div>
     </section>
   )
---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Project1 from "../assets/img/project0.jpeg"
+import Project2 from "../assets/img/project1.jpeg";
+import Project3 from "../assets/img/project2.jpeg";
+import Project4 from "../assets/img/project3.jpeg";
+import Project5 from "../assets/img/project4.jpeg";
+import Project6 from "../assets/img/project5.jpeg";
+
+export const projects = [
+     {
+          title: 'DreamSite',
+          description: 'This is the website of my web development agency.',
+          id:1,
+          image: Project1,
+          url: 'https://euromobila.vercel.app'
+     },
+     {
+          title: 'Website for Euromobila',
+          description: 'This website was created for our friends at Euromobila. They wanted a clean, modern website, so that their clients could easily find their products.',
+          id:2,
+          image: Project2,
+          url: 'https://euromobila.vercel.app'
+     },
+     {
+          title: 'House Marketplace',
+          description: 'This is a house marketplace website, where people can post their houses for sale or rent. It includes authentication, a personal profile where you can manage your listings and many features.',
+          id:3,
+          image: Project3,
+          url:'https://houses-for-sale.vercel.app',
+     }, 
+     {
+          title: 'Landing Page for travel website',
+          description: ' Responsive landing page website made with ReactJs.',
+          id:4,
+          image: Project4,
+          url: "https://vacation-website.vercel.app"
+     },
+     {
+          title: 'Expense Tracker',
+          description: ' Expense tracker app made with ReactJs. Monitor your expenses by adding transactions.',
+          id:5, 
+          image: Project5,
+          url: "https://expensetrack.vercel.app"
+     },
+     {
+          title: 'Photography Portfolio',
+          description: 'This is a photography portfolio. The client wanted a simple website to showcase his work and make it it easier for clients to contact him.',
+          id:6,
+          image: Project6,
+          url: 'https://daniel-s-website.vercel.app'
+     }
+]
